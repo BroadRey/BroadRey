e6ea00c8adc632644589b9fa2bc032cc1a8f7e34
BroadRey <62501467+BroadRey@users.noreply.github.com>
2020-08-05 11:46:16 +0300

    Add files via upload

--- brands.js
+++ brands.js
@@ -10,30 +10,24 @@ let mySwiper = new Swiper('.swiper-container', {
     },
 })
 
-let allBrandLinks = document.querySelectorAll('.brands-container__link');
-//let invisibleBrandLinks = document.querySelectorAll('.brands-container__link:nth-child(n+7)');
 let readMoreText = document.querySelector('.read-more__text');
-let brandsContainer = document.querySelector('.brands-container');
 let brandsWrapper = document.querySelector('.brands-wrapper');
 let readMoreArrows = document.querySelector('.read-more__arrows');
 let readMoreContainer = document.querySelector('.read-more');
+let readMoreButton = document.querySelector('.read-more__button');
 
-readMoreText.addEventListener('click', function() {
-    //invisibleBrandLinks[i].classList.toggle('brands-container__link--invisible');
+readMoreButton.addEventListener('click', function() {
     if (getComputedStyle(brandsWrapper).height === '184px') {
       brandsWrapper.style.height = '272px';
       readMoreContainer.style.paddingTop = '46px';
+      readMoreContainer.style.paddingBottom = '14px';
       readMoreArrows.style.transform = 'rotate(180deg)';
-      readMoreText.textContent = 'Скрыть';
+      readMoreText.textContent = 'Скрыть';  
     } else {
       brandsWrapper.style.height = '184px';
       readMoreArrows.style.transform = 'rotate(0deg)';
       readMoreText.textContent = 'Показать все';
+      readMoreContainer.style.paddingBottom = '34px';
+      readMoreContainer.style.paddingTop = '24px';
     }
-  
-
-  for (let i = 0; i < allBrandLinks.length; i++) {  
-    allBrandLinks[i].classList.toggle('brands-container--appearance');
-  }
-
 });
